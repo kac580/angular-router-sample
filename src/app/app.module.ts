ffbf5a3ea7fdbf73b3573483082e01ab32e77c17
Kyra Buchmann <kac580@outlook.com>
2020-01-28 08:32:14 -0600

    milestone one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent},
+  { path: 'heroes', component: HeroListComponent},
+  { path: '', redirectTo: '/heroes', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent},
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
