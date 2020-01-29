2fa0e4dff8297b6948c3a2869432a1a5b656f7d3
Kyra Buchmann <kac580@outlook.com>
2020-01-28 19:18:44 -0600

    milestone 3 partial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,27 @@ import { FormsModule }    from '@angular/forms';
 
 import { AppComponent }     from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { CrisisListComponent }   from './crisis-list/crisis-list.component';
-import { HeroListComponent }     from './hero-list/hero-list.component';
+// import { HeroListComponent }     from './heroes/hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+// import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HeroesModule,
     AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroListComponent,
+    // HeroListComponent,
     CrisisListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    // HeroDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  selectedHero: Hero;
+
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+}
